7f402f421175bb878ac76251f7783efe4ca321cc
Jon <jon.howell314@gmail.com>
2020-08-16 15:57:03 +0800

    Added score controller

---
+++ NotSuperHex/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+  public HexSpawner spawner;
+
+  public Text text;
+  public int score = 0;
+
+  private void Update()
+  {
+    score = spawner.nCreated;
+    text.text = score.ToString();
+  }
+
+}
